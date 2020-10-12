cf5839add56a32351989b27c3d13214c5c924a1a
kaeptnkrunch <55900780+kaeptnkrunch@users.noreply.github.com>
2020-10-12 10:33:55 +0200

    python-file
    added the right file

---
+++ absenteeism_rate_calculator.py
@@ -0,0 +1,19 @@
+# Library import
+from datetime import date
+                                
+# Greeting
+print("Willkommen zum Fehlzeitenrechner\n")
+
+# Input
+input1 = input("Please indicate your current absences: ")
+input2 = input("Please now enter your current working days: ")
+
+# Date
+currentDate = date.today()
+
+# calculation
+output = (int(input1))/(int(input2))
+output2 = output * 100
+
+# Output
+print("Your current absenteeism rate is " + (str(output2)) + " on " + (str(currentDate)))
