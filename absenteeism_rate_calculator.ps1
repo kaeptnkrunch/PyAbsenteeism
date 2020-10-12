bb20377dde7395037be4b003e990a6ef96334210
kaeptnkrunch <55900780+kaeptnkrunch@users.noreply.github.com>
2020-10-12 10:10:49 +0200

    Create absenteeism_rate_calculator.ps1

---
+++ absenteeism_rate_calculator.ps1
@@ -0,0 +1,22 @@
+﻿# Greeting
+Write-Host "Fehlzeitquoten Rechner von Stephan Langenau"
+
+# Date
+$datum = Get-Date 
+Write-Host "Today we have $datum"
+
+# Break
+Write-Host "`n"
+
+# Input
+$Input1 = Read-Host "Enter your current days of absence"
+$Input2 = Read-Host "Enter your current working days"
+
+# calculation
+$quote = ($Input1 / $Input2) * 100 
+
+# Break
+Write-Host "`n"
+
+# Result
+Write-Host "Deine aktuelle Fehlzeitquote am $datum ist $quote"
